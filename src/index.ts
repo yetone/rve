262b16fa216a3d3b1ec8c48d6ec417718d42920a
yetone <yetoneful@gmail.com>
2019-08-30 15:46:17 +0800

    refactor: eslint instead of tslint

--- src/index.ts
+++ src/index.ts
@@ -8,87 +8,83 @@ const ArrayType = '[object Array]'
 const ComplexTypes = [ObjectType, ArrayType]
 
 interface IAction<T> {
-  state: T,
+    state: T
 }
 
 function isComplexObj(obj: any): boolean {
-  return isComplexType(Object.prototype.toString.call(obj))
+    return isComplexType(Object.prototype.toString.call(obj))
 }
 
 function isComplexType(type: string): boolean {
-  return ComplexTypes.indexOf(type) >= 0
+    return ComplexTypes.indexOf(type) >= 0
 }
 
 function toPlainObj<T>(obj: T): T
 function toPlainObj(obj: any): any {
-  const type = Object.prototype.toString.call(obj)
-  if (type === ObjectType) {
-    return Object.keys(obj).reduce((p, c) => {
-      return {
-        ...p,
-        [c]: toPlainObj(obj[c]),
-      }
-    }, {})
-  }
-  if (type === ArrayType) {
-    return obj.map(toPlainObj)
-  }
-  return obj
+    const type = Object.prototype.toString.call(obj)
+    if (type === ObjectType) {
+        return Object.keys(obj).reduce((p, c) => {
+            return {
+                ...p,
+                [c]: toPlainObj(obj[c]),
+            }
+        }, {})
+    }
+    if (type === ArrayType) {
+        return obj.map(toPlainObj)
+    }
+    return obj
 }
 
 function reducer<T>(_: T, action: IAction<T>) {
-  return action.state
+    return action.state
 }
 
-function value<T>(obj: T): {value: T} {
-  return useState({value: obj})
+function value<T>(obj: T): { value: T } {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    return useState({ value: obj })
 }
 
 function useState<T>(obj: T): T {
-  const [state, dispatch] = useReducer(reducer, obj);
+    const [state, dispatch] = useReducer(reducer, obj)
 
-  return doUseState(state, dispatch, obj, [])
+    return doUseState(state, dispatch, obj, [])
 }
 
 function doUseState<T>(state: T, dispatch: Dispatch<IAction<T>>, obj: T, path: string[]): T {
-  const type = Object.prototype.toString.call(obj)
+    const type = Object.prototype.toString.call(obj)
 
-  const descriptors: PropertyDescriptorMap = Object.keys(obj).reduce((p, c) => {
-    const newPath = [...path, c]
-    let v = objectPath.get(state as any, newPath)
+    const descriptors: PropertyDescriptorMap = Object.keys(obj).reduce((p, c) => {
+        const newPath = [...path, c]
+        let v = objectPath.get(state as any, newPath)
 
-    if (isComplexObj(v)) {
-      v = doUseState(state, dispatch, v, newPath)
+        if (isComplexObj(v)) {
+            v = doUseState(state, dispatch, v, newPath)
+        }
+
+        return {
+            ...p,
+            [c]: {
+                enumerable: true,
+                get() {
+                    return v
+                },
+                set(newV: any) {
+                    newV = toPlainObj(newV)
+                    state = immutable.set(state, newPath, newV)
+
+                    dispatch({
+                        state,
+                    })
+                },
+            },
+        }
+    }, {})
+
+    if (type === ObjectType) {
+        return Object.defineProperties({}, descriptors)
     }
-
-    return {
-      ...p,
-      [c]: {
-        enumerable: true,
-        get() {
-          return v
-        },
-        set(newV: any) {
-          newV = toPlainObj(newV)
-          state = immutable.set(state, newPath, newV)
-
-          dispatch({
-            state,
-          })
-        },
-      },
-    }
-  }, {})
-
-  if (type === ObjectType) {
-    return Object.defineProperties({}, descriptors)
-  }
-  return Object.defineProperties([], descriptors)
+    return Object.defineProperties([], descriptors)
 }
 
-export {
-  value,
-  useState,
-  useEffect,
-  render,
-}
+export { value, useState, useEffect, render }
